f5469c2e80be1a2e31d67b7b86fc639f9949dfcd
ikasafiranasution-hash <ikasafiranasution@gmail.com>
2026-09-16 21:45:18 +0700

    Update oleoleh.cpp

--- oleoleh.cpp
+++ oleoleh.cpp
@@ -17,7 +17,7 @@ int main() {
     }
 
     for (int i = 0; i < 3; i++) {
-        cout << "tabel" << i + 1 << endl;
+        cout << "tabel " << i + 1 << endl;
 
         for (int j = 0; j < 3; j++) {
             for (int k = 0; k < 4; k++) {
