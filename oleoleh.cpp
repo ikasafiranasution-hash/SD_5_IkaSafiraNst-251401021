4f0e08fac66bde1aa705f5d6e50ac76e6f265314
ikasafiranasution-hash <ikasafiranasution@gmail.com>
2026-09-16 21:33:26 +0700

    Add files via upload

---
+++ oleoleh.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    system("cls");
+
+    int arr[3][3][4];
+    int angka = 2;
+
+    // Mengisi array dengan kelipatan 2
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 4; k++) {
+                arr[i][j][k] = angka;
+                angka += 2;
+            }
+        }
+    }
+
+    // Menampilkan array
+    for (int i = 0; i < 3; i++) {
+        cout << "Lapis ke-" << i + 1 << endl;
+
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 4; k++) {
+                cout << arr[i][j][k] << "\t";
+            }
+            cout << endl;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
